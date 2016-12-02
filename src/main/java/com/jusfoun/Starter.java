0ad98cfdccbf070e2bb392036c5a6c8b52425aac
liutiyang <jefferlzu@gmail.com>
2016-12-02 14:44:25 +0800

    增加注解

--- src/main/java/com/jusfoun/Starter.java
+++ src/main/java/com/jusfoun/Starter.java
@@ -1,11 +1,15 @@
 package com.jusfoun;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jusfoun.controller", "com.jusfoun.task"})
 @EnableScheduling
+@EnableAutoConfiguration
 public class Starter {
 
 	public static void main(String[] args) {
