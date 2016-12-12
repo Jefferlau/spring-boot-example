11bf13ad4979c6a5ab3b18a1a7dd614166f0092d
liutiyang <jefferlzu@gmail.com>
2016-12-12 16:13:12 +0800

    增加 Mail 和 Swagger 2 配置示例

--- src/main/java/com/jusfoun/config/WebConfig.java
+++ src/main/java/com/jusfoun/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -27,5 +28,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         // excludePathPatterns 用户排除拦截
         registry.addInterceptor(myInterceptor()).addPathPatterns("/**");
         super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- src/main/java/com/jusfoun/controller/IndexController.java
+++ src/main/java/com/jusfoun/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.jusfoun.controller;
 
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,12 @@ public class IndexController {
     @RequestMapping("/")
     public Map<String, Object> index() {
         return new HashMap<>();
+    }
+
+    @GetMapping("/info")
+    public Map<String, Object> info(@RequestAttribute("name") @ApiParam(value = "名称", required = true) String name) {
+        Map<String, Object> map =  new HashMap<>();
+        map.put("name", name);
+        return map;
     }
 }
